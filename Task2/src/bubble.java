f1bd6afe270415cbadd179184ac91ae8f9ea8a1a
Olga Vasileva <olga.vasileva@t-systems.com>
2019-07-10 17:03:44 +0300

    bubble finished

--- Task2/src/bubble.java
+++ Task2/src/bubble.java
@@ -7,34 +7,39 @@ public class bubble {
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner input = new Scanner(new File("\\home\\olga\\Documents\\list.txt"));
-                //"C:\\Users\\ovasilev\\Documents\\numbers.txt"));
 
+        //System.out.println(Arrays.toString(numbers));
         int size = 10;
-        int[] numbers = new int[size];
+        int[] numbers = new int[10];
         int i;
         for (i = 0; input.hasNext(); i++){
+            if (i == numbers.length) {
+                int[] newNumbers = new int[numbers.length * 2];
+                // copy from the old small table to the new large one
+                System.arraycopy(numbers, 0, newNumbers, 0, i);
+                numbers = newNumbers;
+                System.out.println(Arrays.toString(numbers));
+            }
             numbers[i] = input.nextInt();
         }
-        System.out.println(Arrays.toString(numbers));
 
         int counter = 0;
-        System.out.println(size);
-        while (counter < size-1) {
-            for (i = 0; i < size - 1; ++i) {
+        while (counter < numbers.length) {
+            for (i = 0; i < numbers.length-1; ++i) {
                 if (numbers[i] > numbers[i + 1]) {
                     int tmp = numbers[i];
                     numbers[i] = numbers[i + 1];
                     numbers[i + 1] = tmp;
-                    System.out.println(Arrays.toString(numbers));
+                    //System.out.println(Arrays.toString(numbers));
                 } else {
                     counter++;
-                    System.out.println(counter);
+                    //System.out.println(counter);
                 }
             }
-            if (counter < size -1){
+            if (counter < numbers.length-1){
                 counter = 0;
             }
         }
-
+        System.out.println(Arrays.toString(numbers));
     }
 }
