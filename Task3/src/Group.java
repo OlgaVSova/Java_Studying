327f1731e1e3bef8684457c615d1e7138d8f0ae1
Olga Vasileva <olga.vasileva@t-systems.com>
2019-07-23 20:41:15 +0300

    [+] group

--- Task3/src/Group.java
+++ Task3/src/Group.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.Scanner;
 import java.io.File;
 import java.util.Arrays;
@@ -8,23 +9,41 @@ public class Group {
         Scanner input = new Scanner(new File ("C:\\Users\\ovasilev\\Documents\\users.txt"));
                 //"home/olga/Documents/passengers.txt"));
 
-        int size = 10;
-        User[] users = new User[size];
+//        int size = 10;
+//        User[] users = new User[size];
+//        int i;
+//        for (i = 0; input.hasNext(); i++){
+//            User newUser = new User();
+//            newUser.firstName = input.next();
+//            newUser.secondName = input.next();
+//            if (i == size) {
+//                User[] newUsers = new User[size * 2];
+//                // copy from the old small table to the new large one
+//                System.arraycopy(users, 0, newUsers, 0, i);
+//                users = newUsers;
+//            }
+//            users[i] = newUser;
+//        }
+//        System.out.println(Arrays.toString(users));
+//        System.out.println(users[1]);
+
+//        int i;
+//        for (i = 0; input.hasNext(); i++){
+//            User newUser = new User();
+//            newUser.firstName = input.next();
+//
+//            HashSet userNames = new newUser.firstName();
+// }
+
+        HashSet<User> users = new HashSet<>();
         int i;
         for (i = 0; input.hasNext(); i++){
             User newUser = new User();
-            newUser.firstName = input.next();
-            newUser.secondName = input.next();
-            if (i == size) {
-                User[] newUsers = new User[size * 2];
-                // copy from the old small table to the new large one
-                System.arraycopy(users, 0, newUsers, 0, i);
-                users = newUsers;
-            }
-            users[i] = newUser;
-        }
-        System.out.println(Arrays.toString(users));
-        System.out.println(users[1]);
+//            newUser.firstName = input.next();
+//            newUser.secondName = input.next();
+
+
+         }
 
 
 
@@ -34,12 +53,19 @@ public class Group {
 
 class User{
     String firstName;
-    String secondName;
+    String[] secondNames;
 
     @Override
     public String toString() {
-        return "Mr/Mrs " + firstName +" "+ secondName;
+        return "Mr/Mrs " + firstName +" "+ secondNames;
     }
 }
 
+//class UserList{
+//    String firstName;
+//    String[] secondName;
+//}
+
 //использовать дерево с отсортированным arrayList на листках
+//TreeSet + ArrayList ? Чтобы находить по имени всех пользователей
+// или ArrayList + ArrayList, где каждый нечётный элемент - имя, нечётный - массив фамилий
